0ee5850395ac28fe99d82a425b56f244e28b8705
ScruffyMcGruffy <99762969+ScruffyMcGruffy@users.noreply.github.com>
2023-03-01 11:12:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-#Python code
+#Python code !!!!
+#Import your stufff !!!!!
 from codrone_edu.drone import *
 
 drone = Drone()
+
+color_data = drone.get_color_data
+height = drone.get_height()
+
 drone.pair()
 drone.set_controller_LED(0, 0, 255, 100)
-drone.close()
+
+
+
+while True:
+    time.sleep(0.1)
+    if drone.s_pressed():
+        print(color_data)
+    break
+
+while True:
+    time.sleep(0.1)
+    if drone.h_pressed():
+        print(height)
+    break
